a715e3f49f2a8c97829fc54b77ef0032b70b1142
SkewedCannon <wenzel.aiden@outlook.com>
2024-03-29 11:26:09 -0400

    set the size of the window

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title("<Title>")
 
+# set the size to 640 x 360
+root.minsize(640, 360)
+root.maxsize(640, 360)
+
 button = tk.Button(root, text="Test Button")
 button.place(x=50, y=50)
 
