064caedf95a95bfd7b0cf2faef93ffacd45bece2
SkewedCannon <wenzel.aiden@outlook.com>
2024-03-29 12:25:05 -0400

    added comments

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tkinter as tk
 def print_hello():
     print("Hello world!")
 
+# create and title the window
 root = tk.Tk()
 root.title("Instrument Recognition")
 
@@ -11,9 +12,11 @@ root.title("Instrument Recognition")
 root.minsize(640, 360)
 root.maxsize(640, 360)
 
+# test button
 button = tk.Button(root, text="Test Button", command=print_hello)
 button.place(x=50, y=50)
 
+# make a text box
 test_output = tk.Text(root, font=("Helvetica", "16"), width=49, height=6)
 test_output.place(x=20, y=200)
 
