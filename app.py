74c939273ad7aefb6dea977396d00f9ea361c1d1
SkewedCannon <wenzel.aiden@outlook.com>
2024-03-29 12:24:05 -0400

    set the size and pos of text_output

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def print_hello():
     print("Hello world!")
 
 root = tk.Tk()
-root.title("<Title>") #TODO: replace with app name
+root.title("Instrument Recognition")
 
 # set the size to 640 x 360
 root.minsize(640, 360)
@@ -14,8 +14,8 @@ root.maxsize(640, 360)
 button = tk.Button(root, text="Test Button", command=print_hello)
 button.place(x=50, y=50)
 
-test_output = tk.Text(root, width=20, height=5)
-test_output.place(x = 100, y = 100)
+test_output = tk.Text(root, font=("Helvetica", "16"), width=49, height=6)
+test_output.place(x=20, y=200)
 
 root.mainloop()
 
