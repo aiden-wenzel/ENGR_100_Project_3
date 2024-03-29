d4edeb4bb75b76afbd0def1682a667ce8e8b4873
SkewedCannon <wenzel.aiden@outlook.com>
2024-03-29 11:36:29 -0400

    wrote linked button to print hello to cmd

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import tkinter as tk
 
+# TODO: remove later
+def print_hello():
+    print("Hello world!")
+
 root = tk.Tk()
-root.title("<Title>")
+root.title("<Title>") #TODO: replace with app name
 
 # set the size to 640 x 360
 root.minsize(640, 360)
 root.maxsize(640, 360)
 
-button = tk.Button(root, text="Test Button")
+button = tk.Button(root, text="Test Button", command=print_hello)
 button.place(x=50, y=50)
+
+test_output = tk.Text(root, width=20, height=5)
+test_output.place(x = 100, y = 100)
 
 root.mainloop()
 
