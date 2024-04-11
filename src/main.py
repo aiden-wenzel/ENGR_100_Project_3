195c18dac87d4979d651e255b12bd745c330ef75
Adarsh Bharathwaj <adarshbharathwaj@Adarshs-MacBook-Air.local>
2024-04-11 19:41:52 -0400

    Training CRNN

---
+++ src/main.py
@@ -0,0 +1,23 @@
+from utils import *
+from sklearn.preprocessing import LabelEncoder
+import os
+
+curr = "DIR/sample_audio_training/"
+print(curr)
+
+folders = ['oboe', 'trumpet', 'violin']
+files = []
+labels = []
+
+for folder in folders:
+    folderPath = os.path.join(curr, folder)
+    for filename in os.listdir(folderPath):
+        file_path = os.path.join(folderPath, filename)
+        if os.path.isfile(file_path):  # Make sure it's a file, not a directory or a symlink
+            files.append(file_path)
+            labels.append(folder)
+
+label_encoder = LabelEncoder()
+numeric_labels = label_encoder.fit_transform(labels)
+
+process_and_save_audio(files=files, labels=numeric_labels, output_path="data.npz", sr=22050, add_noise=False)
