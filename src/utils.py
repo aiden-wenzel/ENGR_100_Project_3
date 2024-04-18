ec67488f33f80148875658eb8688d4ee31454912
EmreYavuz45661 <eyavuz45661@gmail.com>
2024-04-18 19:44:05 -0400

    hdf5 fix

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,12 @@ import librosa
 import numpy as np
 from typing import Tuple
 import os
+import logging
+from sklearn.preprocessing import LabelEncoder
+import sys
+import time
+import h5py
+
 
 def combine_tracks(
     path_1: str, path_2: str, mult_1=1, mult_2=1, sr=22050
@@ -34,7 +40,7 @@ def DB_spectogram(y: np.array, sr=22050) -> np.array:
     )
 
 
-def add_gaussian_noise(data, std=0.005):
+def add_gaussian_noise(data: np.array, std=0.005):
     """
     Adds Gaussian noise to an array.
     :param data: numpy array of audio data.
@@ -46,132 +52,131 @@ def add_gaussian_noise(data, std=0.005):
     return data + noise
 
 
-def process_and_save_audio(
+def process_and_save_audio_hdf5(
     files: list, labels: list, output_path: str, sr=22050, add_noise=False
 ):
-    """
-    Process a list of audio files and saves the modified numpy arrays.
-    :param files: List of file paths to the audio files.
-    :param output_path: Path to save the compressed numpy arrays.
-    :param add_noise: Boolean indicating whether to add Gaussian noise.
-    """
     if len(files) != len(labels):
         raise Exception("Length of files must equal labels length")
-    processed_data = {}
 
+    # Creating the actual list of files and labels to process
     files_to_process = []
     labels_to_process = []
-
-    for i in range(len(files)):
-        f = files[i]
+    for i, f in enumerate(files):
         l = labels[i]
         if os.path.isdir(f):
-            dir_files = [
-                os.path.join(dirpath, f)
-                for (dirpath, _, filenames) in os.walk(f)
-                for f in filenames
-            ]
-            files_to_process.extend(dir_files)
-            labels_to_process.extend([l] * len(dir_files))
+            for dirpath, _, filenames in os.walk(f):
+                for filename in filenames:
+                    full_path = os.path.join(dirpath, filename)
+                    files_to_process.append(full_path)
+                    labels_to_process.append(l)
         else:
             files_to_process.append(f)
             labels_to_process.append(l)
 
-    data_means = []
-    data_sizes = []
-    data_variances = []
-    
-    label_size = max(labels) + 1
-    print(label_size)
-
-    for i in range(len(files_to_process)):
-        for j in range(i + 1, len(files_to_process)):
-            m, std, sz = process_and_save_audio_double_helper(
+    # Open a new HDF5 file
+    with h5py.File(output_path, "w") as hdf:
+        
+        label_size = max(labels) + 1
+        progress = 0
+        start_time = time.time()
+        sample_count = 0
+        
+        n = 0
+        mean = 0
+        M2 = 0
+        
+        for i in range(len(files_to_process)):
+            for j in range(i + 1, len(files_to_process)):
+                y, y_labels = process_and_save_audio_helper(
+                    file_path=files_to_process[i],
+                    file_path_2=files_to_process[j],
+                    label=labels_to_process[i],
+                    label_2=labels_to_process[j],
+                    label_size=label_size,
+                    add_noise=add_noise,
+                )
+                if sample_count == 0:
+                    hdf.create_dataset('features', data=y, compression="gzip", chunks=True, maxshape=(None,96,87))
+                    hdf.create_dataset('labels', data=y_labels, compression="gzip", chunks=True, maxshape=(None,3))
+                else:
+                    hdf['features'].resize((hdf['features'].shape[0] + y.shape[0]), axis=0)
+                    hdf['features'][-y.shape[0]:] = y
+                    
+                    hdf['labels'].resize((hdf['labels'].shape[0] + y_labels.shape[0]), axis=0)
+                    hdf['labels'][-y_labels.shape[0]:] = y_labels
+                
+                n += y.size
+                delta = y - mean
+                mean += delta.sum() / n
+                delta2 = y - mean
+                M2 += (delta * delta2).sum()
+                
+                sample_count += y.shape[0]
+            
+            y, y_labels = process_and_save_audio_helper(
                 file_path=files_to_process[i],
-                file_path_2=files_to_process[j],
                 label=labels_to_process[i],
-                label_2=labels_to_process[j],
-                processed_data=processed_data,
-                label_size=label_size
+                label_size=label_size,
+                add_noise=add_noise,
             )
-            data_means.append(m)
-            data_variances.append(std**2)
-            data_sizes.append(sz)
+            hdf['features'].resize((hdf['features'].shape[0] + y.shape[0]), axis=0)
+            hdf['features'][-y.shape[0]:] = y
+            
+            hdf['labels'].resize((hdf['labels'].shape[0] + y_labels.shape[0]), axis=0)
+            hdf['labels'][-y_labels.shape[0]:] = y_labels
+            n += y.size
+            delta = y - mean
+            mean += delta.sum() / n
+            delta2 = y - mean
+            M2 += (delta * delta2).sum()
+            
+            sample_count += y.shape[0]
 
-        m, std, sz = process_and_save_audio_single_helper(
-            file_path=files_to_process[i],
-            label=labels_to_process[i],
-            processed_data=processed_data,
-            label_size=label_size
+            #Track progress
+            if i / len(files_to_process) > progress:
+                time_remaining = (len(files_to_process) - i) * (
+                    time.time() - start_time
+                )/i
+                hours, remainder = divmod(time_remaining, 3600)
+                minutes, seconds = divmod(remainder, 60)
+                # Formatted time output
+                logging.info(
+                    f"\r\n\tTraining data creation progress: [{int(progress*100)}%]\n\tTime remaining: {int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+                )
+                progress += 0.1
+                
+        overall_mean = mean
+        overall_stddev = np.sqrt(M2 / n) if n > 1 else 0.0
+        
+        for i in range(sample_count):
+            hdf['features'][i] = (hdf['features'][i] - overall_mean) / overall_stddev
+                
+        hdf.create_dataset(
+            "overall_metadata", data=np.array([overall_mean, overall_stddev, sample_count])
         )
-        data_means.append(m)
-        data_variances.append(std**2)
-        data_sizes.append(sz)
 
-    overall_mean = sum(mean * size for mean, size in zip(data_means, data_sizes)) / sum(
-        data_sizes
-    )
-    overall_stddev = np.sqrt(
-        (
-            sum(
-                (size - 1) * variance
-                for size, variance in zip(data_sizes, data_variances)
-            )
-            + sum(
-                size * (mean - overall_mean) ** 2
-                for size, mean in zip(data_sizes, data_means)
-            )
-        )
-        / (sum(data_sizes) - 1)
-    )
+        logging.info(f"\n\tTraining data creation progress: [100%]")
 
-    for k in processed_data:
-        if "_data" in k:
-            processed_data[k] = (processed_data[k] - overall_mean) / overall_stddev
-
-    processed_data["overall_metadata"] = np.array([overall_mean, overall_stddev])
-
-    # Save all processed data to a compressed numpy file
-    np.savez_compressed(output_path, **processed_data)
+        # Log progress or any important info
+        logging.info(f"\n\tAudio processing and saving completed successfully.\n\tDataset saved to {output_path} with size {os.path.getsize(output_path)/(1024**2):.2f}MB")
 
 
-def process_and_save_audio_single_helper(
-    file_path: str, label:int, label_size:int, processed_data: dict, sr=22050, add_noise=False
-):
-    audio, _ = librosa.load(file_path, sr=sr)
-    audio = np.pad(audio, (0, sr - (audio.size % sr)), "constant", constant_values=(0))
-
-    # Optionally add Gaussian noise
-    if add_noise:
-        audio = add_gaussian_noise(audio)
-
-    y = DB_spectogram(audio)
-
-    key = get_file_subdir_and_name(file_path)
-    
-    labels = [0]*label_size
-    labels[label] = 1
-
-    # Add to the dictionary with the constructed key
-    for i in range(y.shape[0]):
-        processed_data[key + "_data" + f'_{i}'] = y[i]
-    processed_data[key + "_metadata"] = np.array([np.mean(y), np.std(y), y.size])
-    processed_data[key + "_labels"] = np.array(labels)
-
-    return np.mean(y), np.std(y), y.size
-
-
-def process_and_save_audio_double_helper(
+def process_and_save_audio_helper(
     file_path: str,
-    file_path_2: str,
-    label:int,
-    label_2:int,
-    label_size:int,
-    processed_data: dict,
+    label: int,
+    label_size: int,
+    file_path_2: str = None,
+    label_2: int = None,
     sr=22050,
     add_noise=False,
 ):
-    audio, _ = combine_tracks(file_path, file_path_2, sr=sr)
+    if file_path_2 is not None:
+        audio, _ = combine_tracks(file_path, file_path_2, sr=sr)
+    else:
+        audio, _ = librosa.load(file_path, sr=sr)
+        audio = np.pad(
+            audio, (0, sr - (audio.size % sr)), "constant", constant_values=(0)
+        )
 
     # Optionally add Gaussian noise
     if add_noise:
@@ -179,56 +184,40 @@ def process_and_save_audio_double_helper(
 
     y = DB_spectogram(audio)
 
-    key = (
-        get_file_subdir_and_name(file_path)
-        + "_"
-        + get_file_subdir_and_name(file_path_2)
+    labels = np.full((y.shape[0], label_size), False)
+    labels[:, label] = True
+    if label_2 is not None:
+        labels[:, label_2] = True
+
+    return y, labels
+
+def create_hdf5(
+    Training_Data_Directory: str,
+    Training_Data_Sub_Directories: list,
+    output_path: str,
+    sr=22050,
+    add_noise=False,
+):
+    Training_Data_Folder_Paths = []
+    Training_Data_Folder_Labels = []
+
+    for folder in Training_Data_Sub_Directories:
+        folderPath = os.path.join(Training_Data_Directory, folder)
+        Training_Data_Folder_Paths.append(folderPath)
+        Training_Data_Folder_Labels.append(folder)
+
+    Training_Data_Folder_Numeric_Labels = LabelEncoder().fit_transform(
+        Training_Data_Folder_Labels
     )
 
-    # Add to the dictionary with the constructed key
-    for i in range(y.shape[0]):
-        processed_data[key + "_data" + f'_{i}'] = y[i]
-    processed_data[key + "_metadata"] = np.array([np.mean(y), np.std(y), y.size])
-    
-    labels = [0]*label_size
-    labels[label] = 1
-    labels[label_2] = 1
-    
-    processed_data[key + "_labels"] = np.array(labels)
+    logging.info(
+        f"\nTraining Data\n\tFolders:\n\t\t{Training_Data_Folder_Paths}\n\tRespective Labels:\n\t\t{Training_Data_Folder_Numeric_Labels}"
+    )
 
-    return np.mean(y), np.std(y), y.size
-
-
-def get_file_subdir_and_name(file_path: str) -> str:
-    # Construct a key from the file path to include subdirectory
-    file_path_parts = file_path.split(os.sep)
-    if len(file_path_parts) > 1:
-        name = os.sep.join(
-            file_path_parts[-2:]
-        )  # Last two parts: subdirectory and file name
-    else:
-        name = file_path_parts[0]  # Only the file name, no subdirectory
-
-    return name
-
-
-def load_npz_file_with_condition(file_path, max_size: int):
-    """
-    Loads an .npz file. If the file is over 1GB, it uses mmap_mode='r'.
-
-    Parameters:
-    - file_path: The path to the .npz file.
-
-    Returns:
-    - A dictionary-like object with lazy loading for large files or directly loaded data for smaller files.
-    """
-    file_size = os.path.getsize(file_path)
-
-    if file_size > max_size:
-        print(f"File size is {file_size / (1024**2):.2f}MB. Using mmap_mode='r'.")
-        data = np.load(file_path, mmap_mode="r", allow_pickle=True)
-    else:
-        print(f"File size is {file_size / (1024**2):.2f}MB. Loading normally.")
-        data = np.load(file_path, allow_pickle=True)
-
-    return data
+    process_and_save_audio_hdf5(
+        files=Training_Data_Folder_Paths,
+        labels=Training_Data_Folder_Numeric_Labels,
+        output_path=output_path,
+        sr=sr,
+        add_noise=add_noise,
+    )
