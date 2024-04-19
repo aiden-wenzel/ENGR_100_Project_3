05378a39b6dcdfd41608d88cf993fb5486a6fe04
Adarsh Bharathwaj <adarshbharathwaj@Adarshs-MacBook-Air.local>
2024-04-19 15:30:14 -0400

    CRNN Saved Model

---
+++ src/ML/CRNN/confusion_matrix.py
@@ -0,0 +1,33 @@
+from sklearn.metrics import multilabel_confusion_matrix
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+import torch
+
+class MultiLabelConfusionMatrix:
+    def __init__(self, model, testloader, classes):
+        self.classes = classes
+
+        y_pred = []
+        y_true = []
+
+        for inputs, labels in testloader:
+            output = model(inputs) # feed forward
+            predicted = torch.sigmoid(output) > 0.5
+            y_pred.extend(predicted.numpy().astype(int)) # save predictions
+
+            labels = labels.data.cpu().numpy()
+            y_true.extend(labels.astype(int)) # save truth
+
+        self.confuse_matrices = multilabel_confusion_matrix(y_true, y_pred)
+
+def plot_confusion_matrices(cm: MultiLabelConfusionMatrix, classes: list):
+    for idx, cm in enumerate(cm.confuse_matrices):
+        plt.figure(figsize=(2, 2))
+        sn.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False,
+                xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+        plt.xlabel('Predicted Labels')
+        plt.ylabel('True Labels')
+        plt.title(f'confusion matrix {classes[idx]}')
+        plt.savefig(f'{classes[idx]}.png')
+        plt.close()
