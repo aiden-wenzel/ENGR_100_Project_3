96c48244277c09a8a5d4b62f8951bf60e7650571
Aiden Wenzel <wenzel.aiden@outlook.com>
2024-04-19 14:08:36 -0400

    removed dumn function :(

--- src/main.py
+++ src/main.py
@@ -57,57 +57,6 @@ class MultiLabelCNN(Module):
         
         return x
 
-def initiate_app():
-    
-    # helper functions
-    def print_hello():
-        test_output['state'] = 'normal'
-        test_output.delete('1.0', tk.END)
-        test_output.insert(tk.END, "Bassoon!")
-        test_output['state'] = 'disabled'
-
-    def file_prompt():
-        input_output_box['state'] = 'normal'
-        input_output_box.delete('1.0', tk.END)
-        file_path = filedialog.askopenfilename()
-        # Check if a file was selected
-        if file_path:
-            split_path = file_path.split("/", -1)
-            file = split_path[len(split_path) - 1]
-            input_output_box.insert(tk.END, f"File selected: {file}")
-        else:
-            input_output_box.insert(tk.END, "No file was selected")
-            input_output_box['state'] = 'disabled'
-
-    root = tk.Tk()
-    root.title("Instrument Recognition")
-
-    # set the size to 640 x 360
-    root.minsize(640, 360)
-    root.maxsize(640, 360)
-
-    # test button
-    button = tk.Button(root, text="Test Button", command=print_hello)
-    button.place(x=20, y=180)
-
-    # file prompt button
-    button = tk.Button(root, text="Input File", command=file_prompt)
-    button.place(x=20, y=20)
-
-    # file input text box
-    input_output_box = tk.Text(root, font=("Helvetica", "12"), width=65, height=4)
-    input_output_box.place(x=20, y=60)
-    input_output_box['state'] = 'disabled'
-
-    # make a text box
-    test_output = tk.Text(root, font=("Helvetica", "16"), width=49, height=4)
-    test_output.place(x=20, y=220)
-    test_output['state'] = 'disabled'
-
-    root.mainloop()
-
-
-
 class MILR_APP():
     def __init__(self, root, model):
         self.model = model
@@ -122,7 +71,7 @@ class MILR_APP():
         self.root.maxsize(640, 360)
 
         # test button
-        self.test_button = tk.Button(self.root, text="Test Button", command=self.print_hello)
+        self.test_button = tk.Button(self.root, text="Test Button", command=self.output_predicted_values)
         self.test_button.place(x=20, y=180)
 
         # file prompt button
@@ -139,7 +88,7 @@ class MILR_APP():
         self.test_output.place(x=20, y=220)
         self.test_output['state'] = 'disabled'
 
-    def print_hello(self):
+    def predict_labels(self):
         if (self.file_path == 1):
             print("no file was selected")
         
@@ -149,7 +98,11 @@ class MILR_APP():
     def output_predicted_values(self):
         self.test_output['state'] = 'normal'
         self.test_output.delete('1.0', tk.END)
-        self.test_output.insert(tk.END, "Bassoon!")
+
+        self.predict_labels()
+        for label in self.predicted_labels:
+             self.test_output.insert(tk.END, label)
+
         self.test_output['state'] = 'disabled'
 
     def file_prompt(self):
